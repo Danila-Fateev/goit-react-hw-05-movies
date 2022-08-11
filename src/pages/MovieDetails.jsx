0b9afef85ec4514a25af5b8c15c6639f75fd87d9
Вадим Фатеев <fateevdanon@gmail.com>
2022-08-12 00:22:10 +0200

    Update MovieDetails.jsx

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,1 +1,29 @@
-export default function MovieDetails() {}
+import { useParams } from 'react-router-dom';
+import { useState } from 'react';
+import { useEffect } from 'react';
+export default function MovieDetails() {
+  const { movieId } = useParams();
+  const [movieDetails, setMovieDetails] = useState({});
+
+  useEffect(() => {
+    const fetchMovieDetails = async () => {
+      const fetchedMovieDetails = await fetch(
+        `https://api.themoviedb.org/3/movie/${movieId}?api_key=15d494776c3d35f24fb32811ec733217`
+      )
+        .then(r => r.json())
+        .catch(error => console.log(error));
+
+      setMovieDetails(fetchedMovieDetails);
+    };
+    fetchMovieDetails();
+  }, []);
+
+  const { title, release_date, vote_average, overview, genres } = movieDetails;
+
+  return (
+    <main>
+      <h1></h1>
+      <img src="" alt="" />
+    </main>
+  );
+}
