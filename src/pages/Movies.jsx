fab964a468179d07c4a35f652de540adb98541d5
Вадим Фатеев <fateevdanon@gmail.com>
2022-08-11 23:29:26 +0200

    Update Movies.jsx

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -36,7 +36,7 @@ export default function Movies() {
       <ul>
         {moviesList.map(movie => (
           <li key={movie.id}>
-            <Link to={`movies/${movie.id}`}>{movie.title ?? movie.name}</Link>
+            <Link to={`${movie.id}`}>{movie.title ?? movie.name}</Link>
           </li>
         ))}
       </ul>
