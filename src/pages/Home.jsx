f8035f93e023fc8f35e4c4726662a000c34fd4c3
Вадим Фатеев <fateevdanon@gmail.com>
2022-08-11 22:30:45 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,32 @@
-export default function Home() {
-  const FetchTrendingMovies = () => {
-    console.log('hello');
-  };
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
-  FetchTrendingMovies();
+export default function Home() {
+  const [mainMovieList, setMainMovieList] = useState([]);
+
+  useEffect(() => {
+    const FetchTrendingMovies = async () => {
+      const fetchedMovies = await fetch(
+        'https://api.themoviedb.org/3/trending/all/day?api_key=15d494776c3d35f24fb32811ec733217'
+      )
+        .then(r => r.json())
+        .then(r => r.results)
+        .catch(error => console.log(error));
+      console.log(fetchedMovies);
+      setMainMovieList([...fetchedMovies]);
+    };
+
+    FetchTrendingMovies();
+  }, []);
 
   return (
-    <>
+    <main>
       <h1>Trending today</h1>
-      <ul></ul>
-    </>
+      <ul>
+        {mainMovieList.map(movie => (
+          <li key={movie.id}>{movie.title ?? movie.name}</li>
+        ))}
+      </ul>
+    </main>
   );
 }
