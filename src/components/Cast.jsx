0b88e3419d1e096757f995480792b21e5d211e40
Вадим Фатеев <fateevdanon@gmail.com>
2022-08-12 02:32:34 +0200

    1

--- src/components/Cast.jsx
+++ src/components/Cast.jsx
@@ -1,1 +1,36 @@
-export default function Cast() {}
+import { useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+
+export default function Cast() {
+  const { movieId } = useParams();
+  const [movieCredits, setMovieCredits] = useState([]);
+
+  useEffect(() => {
+    const fetchCredits = async () => {
+      const fetchedCredits = await fetch(
+        `https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=15d494776c3d35f24fb32811ec733217&language=en-US`
+      )
+        .then(r => r.json())
+        .then(r => r.cast);
+      setMovieCredits([...fetchedCredits]);
+    };
+
+    fetchCredits();
+  }, [movieId]);
+
+  return (
+    <ul>
+      {movieCredits.map(actor => (
+        <li key={actor.cast_id}>
+          <img
+            src={`https://image.tmdb.org/t/p/w200${actor.profile_path}` ?? ''}
+            alt={actor.name ?? 'Unknown'}
+          />
+          <p>{actor.name ?? 'Unknown'}</p>
+          <p>Character: {actor.character ?? 'Unknown'}</p>
+          <hr />
+        </li>
+      ))}
+    </ul>
+  );
+}
--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,4 +1,4 @@
-import { useParams } from 'react-router-dom';
+import { useParams, Link, Outlet } from 'react-router-dom';
 import { useState } from 'react';
 import { useEffect } from 'react';
 export default function MovieDetails() {
@@ -15,22 +15,36 @@ export default function MovieDetails() {
 
       setMovieDetails(fetchedMovieDetails);
     };
-    fetchMovieDetails();
-  }, []);
 
-  const { title, release_date, vote_average, overview, genres } = movieDetails;
+    fetchMovieDetails();
+  }, [movieId]);
+
+  const { title, release_date, vote_average, overview, genres, poster_path } =
+    movieDetails;
+  const userScore = Math.round(vote_average * 10);
 
   return (
     <main>
       <h1>
         {title} {release_date?.substring(0, 4)}
       </h1>
-      <p>User score: {`${vote_average * 10}%`}</p>
-      <img src="" alt={title} />
+
+      <img src={`https://image.tmdb.org/t/p/w500${poster_path}`} alt={title} />
+      <p>User score: {`${userScore}%`}</p>
       <h2>Overview</h2>
       <p>{overview}</p>
       <h2>Genres</h2>
       <p>{genres?.map(genre => `${genre.name} `)}</p>
+      <hr />
+      <ul>
+        <li>
+          <Link to="cast">Cast</Link>
+        </li>
+        <li>
+          <Link to="reviews">Reviews</Link>
+        </li>
+      </ul>
+      <Outlet />
     </main>
   );
 }
