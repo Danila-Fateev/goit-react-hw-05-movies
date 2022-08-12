1c833ba5b681f61658ce99c32fb9e6fee420b623
Вадим Фатеев <fateevdanon@gmail.com>
2022-08-12 14:42:22 +0200

    1

--- src/components/GoBackLink.jsx
+++ src/components/GoBackLink.jsx
@@ -1,5 +1,10 @@
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 export default function GoBackLink({ to }) {
   return <Link to={to}>Go back</Link>;
 }
+
+GoBackLink.propTypes = {
+  to: PropTypes.object.isRequired,
+};
