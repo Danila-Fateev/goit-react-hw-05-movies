557c28cffade9ac91d62acb272ce768914b84f06
Вадим Фатеев <fateevdanon@gmail.com>
2022-08-12 02:51:24 +0200

    Update PageNotFound.jsx

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,1 +1,9 @@
-export default function PageNotFound() {}
+import { Link } from 'react-router-dom';
+
+export default function PageNotFound() {
+  return (
+    <main>
+      <h1>Sorry, page not found. {<Link to="/">Go to the main page</Link>}</h1>
+    </main>
+  );
+}
